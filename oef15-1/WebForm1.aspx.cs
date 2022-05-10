e63787147711892c1d5d25fdbb93242379184bc2
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-05-10 09:48:19 +0200

    Add project files.

---
+++ oef15-1/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oef15_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+                Response.Write("Je hebt reeds op VERSTUUR geklikt!");
+            else
+                Response.Write("Klik op VERSTUUR om te versturen!");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
